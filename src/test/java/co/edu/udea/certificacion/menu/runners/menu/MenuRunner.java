ba9a719cd2d7c5840aec190aac4b6e5df52c70ef
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-07-08 19:49:12 -0500

    runner implementado

--- src/test/java/co/edu/udea/certificacion/menu/runners/menu/MenuRunner.java
+++ src/test/java/co/edu/udea/certificacion/menu/runners/menu/MenuRunner.java
@@ -1,4 +1,4 @@
-package co.edu.udea.certificacion.menu.runners;
+package co.edu.udea.certificacion.menu.runners.menu;
 
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/resources/features/.../",
-        glue = "co.edu.udea.certificacion.stepdefinitions",
+        features = "src/test/resources/features/menu/menu_navigation.feature",
+        glue = "co.edu.udea.certificacion.menu.stepdefinitions",
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
-public class Runner {}
+public class MenuRunner {}

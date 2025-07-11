75cdb798d10f3d810f539226f8401418bbd77b35
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-07-10 23:07:44 -0500

    mejroando codigo con actores

--- src/test/java/co/edu/udea/certificacion/menu/stepdefinitions/MenuNavigationStepDefinition.java
+++ src/test/java/co/edu/udea/certificacion/menu/stepdefinitions/MenuNavigationStepDefinition.java
@@ -12,65 +12,62 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
-import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class MenuNavigationStepDefinition {
+
+    private static final String ACTOR_NAME = "cliente";
+    private Actor cliente;
 
     @Before
     public void setTheStage() {
         OnStage.setTheStage(new OnlineCast());
+        cliente = OnStage.theActorCalled(ACTOR_NAME);
     }
 
     @Given("que he iniciado sesion como usuario estandar")
     public void queHeIniciadoSesionComoUsuarioEstandar() {
-        OnStage.theActorCalled("usuario").attemptsTo(
-                LoginToSauceDemo.loginToSauceDemo()
-        );
+        cliente.attemptsTo(
+                LoginToSauceDemo.loginToSauceDemo());
     }
 
     @When("abro el menu y selecciono la opcion About")
     public void abroElMenuYSeleccionoAbout() {
-        theActorInTheSpotlight().attemptsTo(
-                NavigateToAbout.navigateToAbout()
-        );
+        cliente.attemptsTo(
+                NavigateToAbout.navigateToAbout());
     }
 
     @Then("puedo ver la seccion About")
     public void puedoVerLaSeccionAbout() {
-        theActorInTheSpotlight().should(
-                seeThat(ValidationAboutOption.onPage())
-        );
+        cliente.should(
+                seeThat(ValidationAboutOption.onPage()));
     }
 
     @When("abro el menu y selecciono la opcion Logout")
     public void abroElMenuYSeleccionoLogout() {
-        theActorInTheSpotlight().attemptsTo(
-                NavigateToLogout.navigateToLogout()
-        );
+        cliente.attemptsTo(
+                NavigateToLogout.navigateToLogout());
     }
 
     @Then("debo volver a la pagina de inicio de sesion")
     public void deboVolverALaPaginaDeInicioDeSesion() {
-        theActorInTheSpotlight().should(
-                seeThat(ValidationLoginPage.onPage())
-        );
+        cliente.should(
+                seeThat(ValidationLoginPage.onPage()));
     }
 
     @When("agrego un producto al carrito y abro el menu y selecciono la opcion Reset App State")
     public void agregoProductoYSeleccionoResetAppState() {
-        theActorInTheSpotlight().attemptsTo(
+        cliente.attemptsTo(
                 AddProductToCart.addProductToCart(),
-                NavigateToResetAppState.navigateToResetAppState()
-        );
+                NavigateToResetAppState.navigateToResetAppState());
     }
 
     @Then("el carrito debe estar vacio y los botones de los productos deben estar en Add to cart")
     public void elCarritoDebeEstarVacioYLosBotonesDebenEstarEnAddToCart() {
-        theActorInTheSpotlight().should(
-                seeThat(ValidationResetAppState.onPage())
-        );
+        cliente.should(
+                seeThat(ValidationResetAppState.onPage()));
     }
 }
